b3b2afcbb2ae0a5e3d10acdb01eb2b37aeb85c51
Glassig <angelina.v.gegerfelt@gmail.com>
2020-05-20 23:16:14 +0200

    Added mapbox map to application.

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,16 +1,39 @@
 import React from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import MapboxGL from "@react-native-mapbox-gl/maps";
 
 import Layout from "./Layout";
-import MText from "./../components/Utils/MText";
 import { skyBlue } from "../styling/colors";
 import Mountains from "./../styling/images/map.svg";
+import { MAPBOX_KEY } from "./../../.env.js";
 
+MapboxGL.setAccessToken(MAPBOX_KEY);
+MapboxGL.setConnected(true);
 const Map = ({ navigation }) => {
   return (
     <Layout gradient={[skyBlue.dark, skyBlue.light]} mountainHeight={198} Mountains={Mountains}>
-      <MText>Map stuff</MText>
+      <View style={styles.page}>
+        <View style={styles.container}>
+          <MapboxGL.MapView style={styles.map} logoEnabled={false} />
+        </View>
+      </View>
     </Layout>
   );
 };
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    alignItems: "center",
+  },
+  container: {
+    height: Dimensions.get("window").height - 198 - 60, // 198 for mountains, and 60 because random
+    width: Dimensions.get("window").width - 40, // padding 20
+    backgroundColor: "tomato",
+  },
+  map: {
+    flex: 1,
+  },
+});
 
 export default Map;
